7e4c56d1f142b63460f9ef001e60dc25e4da7311
Peter Abplanalp <peter@abplanalps.net>
2012-05-11 08:27:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MileageLog::Application.config.secret_token = 'a2c36cdc34c02d62eb20abc751222d849b9217148d0b013473ebeb38581c66574f5821904e1c812b052c7040ec423910c5076b5536dbf20c930e3b82555050ca'
